96f5880cf3b136232b9dd853974cce1a6c9a74b2
PabloRestrepo01 <70071301+PabloRestrepo01@users.noreply.github.com>
2022-01-25 11:34:12 -0500

    Create empleado.py

---
+++ Practica_2/personas/empleado.py
@@ -0,0 +1,9 @@
+class Empleado:
+    
+    def __init__(self, cedula, nombre, sueldo):
+        self._cedula = cedula
+        self._nombre = nombre
+        self._sueldo = sueldo
+    
+    def __str__():
+        pass
